e809b70cd43f96411785c9059df4ca61b8962958
dapid1 <71635735+dapid1@users.noreply.github.com>
2021-08-18 12:22:38 +0200

    KarateChop Code Kata

---
+++ KarateChop/KarateChop/KarateChop.cs
@@ -0,0 +1,12 @@
+﻿namespace KarateChop
+{
+    class KarateChop
+    {
+        public int Count { get; set; }
+        //Iterative approach
+        public int Chop(int target, int[] array)
+        {
+            return -1;
+        }
+    }
+}
---
+++ KarateChop/KarateChopTesting/UnitTest1.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KarateChopTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Empty dataset
+        //For each test test efficiency (count)
+        //Middle Value (Found)
+        //First Value
+        //Last Value
+        //negative values
+        //duplicate values
+        //Not found
+        [TestMethod]
+        public void EmptyDataSet()
+        {
+
+        }
+        [TestMethod]
+        public void MiddleValue()
+        {
+
+        }
+        [TestMethod]
+        public void FirstValue()
+        {
+
+        }
+        [TestMethod]
+        public void LastValue()
+        {
+
+        }
+        [TestMethod]
+        public void GreaterThanMiddle()
+        {
+
+        }
+        [TestMethod]
+        public void LessThanMiddle()
+        {
+
+        }
+        [TestMethod]
+        public void RandomValue()
+        {
+
+        }
+        //
+        [TestMethod]
+        public void VeryHighValue()
+        {
+
+        }
+    }
+}

a8a180a3496dc2c25b0539a94e6c74a44f4471f6
dapid1 <71635735+dapid1@users.noreply.github.com>
2021-08-19 12:48:28 +0200

    Updated Unit tests

---
+++ KarateChop/KarateChop/KarateChop/KarateChopBS.cs
@@ -0,0 +1,32 @@
+﻿namespace KarateChop
+{
+    public class KarateChopBS
+    {
+        public int Count { get; set; } = 0;
+        //Iterative approach
+        public int Chop(int target, int[] array)
+        {
+            Count = 0;
+            //Two pointer
+            //p1 first index
+            //p2 last index
+            int p1 = 0;
+            int p2 = array.Length - 1;
+            while (p1 <= p2)
+            {
+                Count++;
+                //get mid index
+                int mid = p1 + ((p2 - p1) / 2);
+                int val = array[mid];
+                if (target == val)
+                    return mid;
+                //move pointer
+                if (target > val)
+                    p1 = mid + 1;
+                else
+                    p2 = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
